f5dafa9124d025007eb4e2cc1928707e4a806e5c
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2020-09-16 14:58:32 +0200

    Add files via upload

---
+++ ConsoleApps/ConsoleTest/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleTest.Models
+{
+    public class User
+    {
+
+        public string Name;
+        public int Age;
+        public string City;
+                
+        public User()
+        {
+            
+        }
+    }
+}
---
+++ ConsoleApps/ConsoleTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ConsoleTest.Models;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsa whats your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hvor kommer du fra?");
+            string by = Console.ReadLine();
+
+            Console.WriteLine("Hvor gammel er du?");
+            int age = CalculateAge(Console.ReadLine());
+
+            User myUser = new User();
+            myUser.Name = name;
+            myUser.City = by;
+            myUser.Age = age;
+
+            Console.WriteLine($"Du hedder {myUser.Name}, Du kommer fra {myUser.City} og næste år bliver du {myUser.Age + 1} år gammel");
+        }
+
+        protected static int CalculateAge(string age)
+        {
+            int result = 0;
+            // vi forsøger at parse brugerens input til int
+            try
+            {
+                result = int.Parse(age);
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+    }
+}
